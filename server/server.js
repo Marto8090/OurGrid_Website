290c6fe51877f9d38b922f26b31643cc9e7e087e
Martin Fakirov <546016@student.fontys.nl>
2025-10-27 15:07:57 +0100

    fixed the the server to be able to run

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import data from "./data/demoData.json" assert { type: "json" };
+const express = require("express");
+const cors = require("cors");
+const data = require("./data/demoData.json");
 
 const app = express();
 app.use(cors());
