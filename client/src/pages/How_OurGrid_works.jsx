1cb0bea01a8b54ade71477c5d2e736826a829b7f
Martin Fakirov <546016@student.fontys.nl>
2025-12-02 16:37:00 +0100

    Finished the 4 step of the how our grid works

--- client/src/pages/How_OurGrid_works.jsx
+++ client/src/pages/How_OurGrid_works.jsx
@@ -2,8 +2,10 @@ import { useAudience } from "../context/AudienceContext.jsx";
 import RegisterPage from "../assets/RegisterPage.jpg";
 import PeakCongestion from "../assets/PeakCongestion.jpg";
 import ChallengePopUp from "../assets/ChallengePopUp.jpg";
+import TipsActions from "../assets/Tips_Actions.jpg";
 import dongle from "../assets/dongle.png";
 import powerBrick from "../assets/power brick.png";
+import energyCables from "../assets/energyCables.png";
 import { Cog } from "lucide-react";
 
 export default function HowOurGridWorks() {
@@ -121,7 +123,7 @@ export default function HowOurGridWorks() {
 
             <div className="relative p-5 text-center space-y-4">
               {/* top-left curve */}
-              <div className="absolute top-0 left-0 w-40 h-20 
+              <div className="absolute top-0 left-0 w-30 h-20 
                   border-t-2 border-l-2 border-[#4F2E39] rounded-tl-4xl"></div>
 
               {/* top-right curve */}
@@ -150,18 +152,118 @@ export default function HowOurGridWorks() {
         {/* STEP 4 */}
         <div className="bg-[#4F2E39] text-[#F9F5F2] rounded-2xl shadow-md border-2 border-[#F4B14A] rounded-b-2xl md:rounded-2xl overflow-hidden">
           <div className="px-5 pt-6 pb-5 space-y-4">
-            <div className="w-10 h-10 mx-auto rounded-full border-2 border-[#F9F5F2] flex items-center justify-center text-lg font-bold">
+            <div className="w-14 h-14 mx-auto rounded-full border-2 border-[#F9F5F2] flex items-center justify-center text-2xl font-bold">
               4
             </div>
-            <h2 className="text-lg font-semibold text-center">
+            <h2 className="text-2xl font-semibold text-center">
               Follow tips & take action
             </h2>
 
-            <p className="text-sm leading-relaxed text-center mt-2 text-[#F9F5F2]/90">
-              During a challenge, the app gives practical actions:
-              delay EV charging, lower heating, discharge your battery, or
-              postpone laundry. Each action helps lower the load.
-            </p>
+            {/* PHONE WITH FADE */}
+            <div className="relative w-fit mx-auto pt-4">
+              <img src={TipsActions} alt="OurGrid login screen"
+                className="w-48 h-70 object-cover rounded-2xl border-[1px] border-[#000000]"/>
+              <div className="absolute -bottom-1 left-0 w-full h-40 bg-gradient-to-b from-transparent via-[#4F2E39]/90 to-[#4F2E39]"></div>
+            </div>
+            <div className="space-y-4 text-[#F9F5F2]/90">
+              {/* Intro text */}
+              <p className="text-lg leading-relaxed">
+                During a challenge, the app gives practical actions:
+              </p>
+              <div className="relative">
+                {/* background image */}
+                <img src={energyCables} className="absolute -bottom-20 -right-8 w-40 opacity-80 object-contain pointer-events-none select-none"/>
+                {/* Bullet list */}
+                <ul className="space-y-4">
+                  {/* ITEM 1 */}
+                  <li className="flex items-start gap-3">
+                    <span className="text-[#F4B14A] text-xl leading-none mt-1">•</span>
+
+                    <span className="relative inline-block px-3 py-1">
+                      <span className="relative z-10 text-lg leading-relaxed">
+                        Delay charging your EV
+                      </span>
+                      {/* BOTTOM — full width */}
+                      <span className="absolute inset-y-0 right-0 w-1/2 
+                        border-b-2 border-r-2 
+                        border-[#F4B14A] rounded-r-full -z-0">
+                      </span>
+                      {/* TOP — shorter width */}
+                      <span className="absolute top-0 right-0 h-[17px] w-1/3 
+                        border-t-2 border-r-2 
+                        border-[#F4B14A] rounded-tr-full -z-0">
+                      </span>
+                    </span>
+                  </li>
+
+                  {/* ITEM 2 */}
+                  <li className="flex items-start gap-3">
+                    <span className="text-[#F4B14A] text-xl leading-none mt-1">•</span>
+
+                    <span className="relative inline-block px-3 py-1">
+                      <span className="relative z-10 text-lg leading-relaxed">
+                        Lower heating
+                      </span>
+                      {/* BOTTOM — full width */}
+                      <span className="absolute inset-y-0 right-0 w-1/2 
+                        border-b-2 border-r-2 
+                        border-[#F4B14A] rounded-r-full -z-0">
+                      </span>
+                      {/* TOP — shorter width */}
+                      <span className="absolute top-0 right-0 h-[17px] w-1/3 
+                        border-t-2 border-r-2 
+                        border-[#F4B14A] rounded-tr-full -z-0">
+                      </span>
+                    </span>
+                  </li>
+
+                  {/* ITEM 3 */}
+                  <li className="flex items-start gap-3">
+                    <span className="text-[#F4B14A] text-xl leading-none mt-1">•</span>
+
+                    <span className="relative inline-block px-3 py-1">
+                      <span className="relative z-10 text-lg leading-relaxed">
+                        Discharge your battery
+                      </span>
+
+                      {/* BOTTOM — full width */}
+                      <span className="absolute inset-y-0 right-0 w-3/5 
+                        border-b-2 border-r-2 
+                        border-[#F4B14A] rounded-r-full -z-0">
+                      </span>
+                      {/* TOP — shorter width */}
+                      <span className="absolute top-0 right-0 h-[17px] w-1/3 
+                        border-t-2 border-r-2 
+                        border-[#F4B14A] rounded-tr-full -z-0">
+                      </span>
+                    </span>
+                  </li>
+
+                  {/* ITEM 4 */}
+                  <li className="flex items-start gap-3">
+                    <span className="text-[#F4B14A] text-xl leading-none mt-1">•</span>
+
+                    <span className="relative inline-block px-3 py-1">
+                      <span className="relative z-10 text-lg leading-relaxed">
+                        Postpone laundry
+                      </span>
+                      {/* BOTTOM — full width */}
+                      <span className="absolute inset-y-0 right-0 w-2/3 
+                        border-b-2 border-r-2 border-[#F4B14A] rounded-r-full -z-0">
+                      </span>
+                      {/* TOP — shorter width */}
+                      <span className="absolute top-0 right-0 h-[17px] w-1/3 
+                        border-t-2 border-r-2 border-[#F4B14A] rounded-tr-full -z-0">
+                      </span>
+                    </span>
+                  </li>
+                </ul>
+              </div>
+              {/* Closing text */}
+              <p className="text-lg leading-relaxed">
+                Each action shows how much power it saves and helps you reach your goal faster.
+              </p>
+            </div>
           </div>
         </div>
 
